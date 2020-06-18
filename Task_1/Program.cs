1cbb399e886562041bc3cc0382279bfd72c155d4
MladjanMrksic <mladjanmrksic@gmail.com>
2020-06-18 08:45:13 +0200

    Created project, added basic logic behind thread creation

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task_1
@@ -10,6 +11,26 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("/t/t/tWelcome to BankSimulation");
+            int firstLine;
+            do
+            {
+                Console.WriteLine("Please enter the number of people in the first line");
+            } while (int.TryParse(Console.ReadLine(),out firstLine) == false);
+            int secondLine;
+            do
+            {
+                Console.WriteLine("Please enter the number of people in the second line");
+            } while (int.TryParse(Console.ReadLine(), out secondLine) == false);
+            Thread t;
+            List<Thread> threadList = new List<Thread>();
+            for (int i = 1; i <= firstLine+secondLine; i++)
+            {
+                t = new Thread(DoSmth);
+                t.Name = string.Format("Thread_" + i);
+                threadList.Add(t);
+            }
         }
+        public static void DoSmth() { }
     }
 }
