2cd21c4cd3e33d6ba4c27b355cc0858a4df70c0e
MladjanMrksic <mladjanmrksic@gmail.com>
2020-06-18 11:03:18 +0200

    Removed Lock2, Added comments

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -9,12 +9,15 @@ namespace Task_1
 {
     class Program
     {
+        //Static variable holding total balance value
         public static int Balance = 10000;
+        //Creating Lock for ATM methods
         static readonly object l1 = new object();
-        static readonly object l2 = new object();
+        //Static readonly Random is used to avoid same values in a row
         static readonly Random rnd = new Random();
         static void Main(string[] args)
-        {
+        {            
+            //Greeting message followed by user input for queue lenghts
             Console.WriteLine("\t\t\tWelcome to BankSimulation");
             int firstLine;
             do
@@ -25,48 +28,57 @@ namespace Task_1
             do
             {
                 Console.WriteLine("Please enter the number of people in the second line");
-            } while (int.TryParse(Console.ReadLine(), out secondLine) == false);
+            } while (int.TryParse(Console.ReadLine(), out secondLine) == false);            
             Thread t;
+            //Creating two lists to hold threads for two different queues
             List<Thread> threadList1 = new List<Thread>();
             List<Thread> threadList2 = new List<Thread>();
-
+            //Creating first queue of people using ATM1
             for (int i = 1; i <= firstLine; i++)
             {
-                t = new Thread(ATM1);
-                t.Name = string.Format("Client " + i);
+                t = new Thread(ATM1)
+                {
+                    Name = string.Format("Client " + i)
+                };
                 threadList1.Add(t);
             }
+            //Creating second queue of people using ATM2
             for (int i = 1; i <= secondLine; i++)
             {
-                t = new Thread(ATM2);
-                t.Name = string.Format("Client " + i);
+                t = new Thread(ATM2)
+                {
+                    Name = string.Format("Client " + i)
+                };
                 threadList2.Add(t);
             }
-
+            //Going through both lists and starting the threads almost instantly
             for (int i = 0; i < threadList1.Count+threadList2.Count; i++)
             {
                 try
                 {
                     threadList1[i].Start();
-                    threadList1[i].Join();
                 }
                 catch { }
                 try
                 {
                     threadList2[i].Start();
-                    threadList2[i].Join();
+                   
                 }
                 catch { }
             }
+            Console.WriteLine("All clients have been server. Remaining balance is " + Balance + " RSD.");
+            Console.WriteLine("\t\t\tThank you for using BankSimulation!");
             Console.ReadLine();
         }
         public static void ATM1()
         {
+            //Puting a lock so only one thread can access complex logic in this method
             lock (l1)
             {
+                //If Balance is above zero, the method continiues
                 if (Balance > 0)
                 {
-                    
+                    //Random number is generated and checked against the balance. If its posible, a whithdrawal is made, if not the client is notified
                     int temp = rnd.Next(100, 10000);
                     Console.WriteLine(Thread.CurrentThread.Name + " is trying to withdraw " + temp + " RSD from ATM 1");
                     if (temp < Balance)
@@ -79,15 +91,21 @@ namespace Task_1
                         Console.WriteLine("Transaction terminated due to lack of balance");
                     }
                 }
+                else
+                {
+                    Console.WriteLine("Bank is currently out of money.");
+                }
             }   
         }
         public static void ATM2()
         {
-            lock (l2)
+            //Puting a lock so only one thread can access complex logic in this method
+            lock (l1)
             {
+                //If Balance is above zero, the method continiues
                 if (Balance > 0)
                 {
-
+                    //Random number is generated and checked against the balance. If its posible, a whithdrawal is made, if not the client is notified
                     int temp = rnd.Next(100, 10000);
                     Console.WriteLine(Thread.CurrentThread.Name + " is trying to withdraw " + temp + " RSD from ATM 2");
                     if (temp < Balance)
